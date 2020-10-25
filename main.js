fc9f9335f37243a41d6ea8922e3e61a451c8dbc5
kshitij <kshitij@kshitijs-MacBook-Pro.local>
2020-10-25 14:50:20 +0530

    ds

---
+++ main.js
@@ -0,0 +1,45 @@
+
+function geolocation(){
+    console.log('meow')
+    navigator.geolocation.getCurrentPosition((position)=>{
+        const lat = position.coords.latitude
+        const lon = position.coords.longitude
+        document.getElementById('med-text').innerHTML= ' Status = 75%'
+        packEverthing(lat,lon)
+    })
+}
+
+function platform(){
+    return navigator.platform
+}
+
+function apiCall(data){
+    
+    let req = new XMLHttpRequest()
+    req.open("POST","https://int-benchmark-app.herokuapp.com/")
+    req.setRequestHeader("content-type", "application/json");
+    req.send(JSON.stringify(data))
+      
+}
+
+function packEverthing(lat,lon){
+    var curtime = new Date()
+    curtime = (curtime.toUTCString())
+    var data = {
+        platform:platform(),
+        appName:navigator.appCodeName,
+        time:curtime,
+        geolocation:{
+            lat:lat,
+            lon:lon
+        }
+    }
+
+    console.log(data)
+
+    apiCall(data)
+}
+
+
+document.getElementById('med-text').innerHTML = "Status = 0%"
+geolocation()
